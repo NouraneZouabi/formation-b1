c821e030d668cb5ce5257dee56015ec6df411f19
Nourane Zouabi <100202617+NouraneZouabi@users.noreply.github.com>
2026-03-06 23:21:40 +0100

    update

--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,4 +1,7 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://54.175.125.22:31321'
-};
+export const environment :
+{
+  production?:boolean,
+}
+= {
+    production: false,
+  };
